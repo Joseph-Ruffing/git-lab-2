ce006ca60417afbe74f825c9321f4bd7ed9605a2
Joseph-Ruffing <jr684124@ohio.edu>
2024-09-06 15:19:40 -0400

    the program file

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Joey Ruffing
+ *        Date: 9/6/24
+ * Description: git lab program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
