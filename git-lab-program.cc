e55b21200e1c2c1d0c8680ffb13940412226c4b0
Joseph Ruffing <jr684124@ohio.edu>
2025-05-13 11:48:49 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Joey Ruffing
+ *        Date: 5/13/2025
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
